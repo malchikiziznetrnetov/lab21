7d24da6fcb68ed4256876091c6c4afba69ef3c3a
malchikiziznetrnetov <xAkaki1337@gmail.com>
2024-06-24 14:25:01 +0300

    Добавился multiply

--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace suka
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выберите действие: +, -, *, /, e - выйти");
+            bool checker = true;
+            while (checker == true)
+            {
+                try
+                {
+                    char symbol = char.Parse(Console.ReadLine());
+                    switch (symbol)
+                    {
+                        case '*':
+                            Console.WriteLine($"Результат {a * b}");
+                            break;
+                        default:
+                            Console.WriteLine("Выберите действие: +, -, *, /, e - выйти");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
